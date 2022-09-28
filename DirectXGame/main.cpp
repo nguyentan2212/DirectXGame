666c5db9bb7ee4873b5915defdac4fa38d9c10b8
nguyentan2212 <nguyentan2212@gmail.com>
2022-09-28 12:53:24 +0700

    init project

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,16 @@
+#include <Windows.h>
+#include <string>
+using namespace std;
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+
+	// Register the window class.
+	string className = "My Game";
+
+	return 0;
+}
