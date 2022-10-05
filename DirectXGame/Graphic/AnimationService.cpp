3fef47d3281bb78652e696ac2e2dd5447fd47f41
nguyentan2212 <nguyentan2212@gmail.com>
2022-10-05 12:41:27 +0700

    add service for animation

---
+++ DirectXGame/Graphic/AnimationService.cpp
@@ -0,0 +1,68 @@
+#include "AnimationService.h"
+#include <fstream>
+#include "../Utils/Debug.h"
+
+AnimationService* AnimationService::_instance = nullptr;
+
+AnimationService::AnimationService()
+{
+
+}
+
+AnimationService* AnimationService::GetInstance()
+{
+    if (_instance == nullptr)
+    {
+        _instance = new AnimationService();
+    }
+    return _instance;
+}
+
+void AnimationService::Init(json config)
+{
+    for (json item : config)
+    {
+        string animationPath = item["animationPath"].get<string>();
+        fstream file(animationPath);
+
+        json fileJson = json::parse(file);
+        json animationsJson = fileJson["animations"];
+
+        for (json ani : animationsJson)
+        {
+            string animationName = ani["name"].get<string>();
+            DWORD defaultTime = ani["defaultTime"].get<DWORD>();
+            Animation* animation = new Animation(defaultTime);
+
+            json frames = ani["frames"];
+            for (json frame : frames)
+            {
+                string spriteName = frame["sprite"].get<string>();
+                if (frame["time"].is_null())
+                {
+                    animation->AddFrame(spriteName);
+                }
+                else
+                {
+                    DWORD time = frame["time"].get<DWORD>();
+                    animation->AddFrame(spriteName, time);
+                }
+            }
+            this->_animations[animationName] = animation;
+        }
+    }
+}
+
+Animation* AnimationService::GetAnimation(string name)
+{
+    Animation* animation = this->_animations[name];
+    if (animation == nullptr)
+    {
+        DebugOut(L"[ERROR] Sprite name: '%s' not found !\n", name);
+    }
+    return animation;
+}
+
+AnimationService::~AnimationService()
+{
+}
---
+++ DirectXGame/Graphic/AnimationService.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <map>
+#include <string>
+#include "Animation.h"
+#include "../Utils/json.hpp"
+
+using namespace::std;
+using json = nlohmann::json;
+
+class AnimationService
+{
+public:
+	static AnimationService* GetInstance();
+
+	void Init(json config);
+	Animation* GetAnimation(string name);
+
+private:
+	AnimationService();
+	~AnimationService();
+	map<string, Animation*> _animations;
+
+	static AnimationService* _instance;
+};
+
