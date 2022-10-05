af23f8174ffa7ba295878e0dba1045784ec49d8c
nguyentan2212 <nguyentan2212@gmail.com>
2022-10-05 12:40:52 +0700

    add animation class

---
+++ DirectXGame/Graphic/Animation.cpp
@@ -0,0 +1,43 @@
+#include "Animation.h"
+#include "SpriteService.h"
+#include "../Utils/Debug.h"
+
+Animation::Animation(DWORD defaultTime)
+{
+	this->_defaultTime = defaultTime;
+	this->_currentFrame = 0;
+	this->_totalDelayTime = 0;
+}
+
+void Animation::AddFrame(string spriteName, DWORD time)
+{
+	SpriteService* sprites = SpriteService::GetInstance();
+	Sprite* sprite = sprites->GetSprite(spriteName);
+	AddFrame(sprite, time);
+}
+
+void Animation::AddFrame(Sprite* sprite, DWORD time)
+{
+	time = time > 0 ? time : this->_defaultTime;
+
+	AnimationFrame* frame = new AnimationFrame(sprite, time);
+
+	this->_frames.push_back(frame);
+}
+
+void Animation::Update(float deltaTime)
+{
+	this->_totalDelayTime += deltaTime;
+
+	DWORD time = this->_frames[this->_currentFrame]->time;
+	if (this->_totalDelayTime > time)
+	{
+		this->_totalDelayTime = 0.0f;
+		this->_currentFrame = (this->_currentFrame + 1) % this->_frames.size();
+	}
+}
+
+void Animation::Render(float x, float y)
+{
+	this->_frames[this->_currentFrame]->sprite->Draw(x, y);
+}
---
+++ DirectXGame/Graphic/Animation.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "AnimationFrame.h"
+#include "Sprite.h"
+
+using namespace::std;
+
+class Animation
+{
+public:
+	Animation(DWORD defaultTime);
+
+	void AddFrame(string spriteName, DWORD time = 0);
+	void AddFrame(Sprite* sprite, DWORD time = 0);
+
+	void Update(float deltaTime);
+	void Render(float x, float y);
+
+private:
+	DWORD _defaultTime;
+	int _currentFrame;
+	float _totalDelayTime;
+	vector<AnimationFrame*> _frames;
+};
+
