71afdc795ba30528bd8a6ebf784795719bf645f8
nguyentan2212 <nguyentan2212@gmail.com>
2022-10-08 13:21:27 +0700

    add vector class

---
+++ DirectXGame/Math/Vector.cpp
@@ -0,0 +1,34 @@
+#include "Vector.h"
+
+Vector::Vector()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		this->_vector[i] = 0;
+	}
+}
+
+Vector::Vector(float x, float y, float z)
+{
+	this->_vector[0] = x;
+	this->_vector[1] = y;
+	this->_vector[2] = z;
+	this->_vector[3] = 0;
+}
+
+float Vector::getLength()
+{
+	float x2 = this->_vector[0] * this->_vector[0];
+	float y2 = this->_vector[1] * this->_vector[1];
+	float z2 = this->_vector[2] * this->_vector[2];
+
+	return sqrtf(x2 + y2 + z2);
+}
+
+void Vector::operator=(const Vector& vec)
+{
+	this->_vector[0] = vec._vector[0];
+	this->_vector[1] = vec._vector[1];
+	this->_vector[2] = vec._vector[2];
+	this->_vector[3] = vec._vector[3];
+}
