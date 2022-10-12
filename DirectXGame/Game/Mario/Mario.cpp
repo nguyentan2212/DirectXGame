bd3f9214995c3720999f131e69dd1b15f089b0fb
nguyentan2212 <nguyentan2212@gmail.com>
2022-10-12 18:07:15 +0700

    handle input and process state commands

--- DirectXGame/Game/Mario/Mario.cpp
+++ DirectXGame/Game/Mario/Mario.cpp
@@ -1,13 +1,69 @@
 #include "Mario.h"
 #include "MarioRunState.h"
 #include "MarioSitState.h"
+#include "MarioIdleState.h"
 
-Mario::Mario(): GameObject(new MarioRunState())
+Mario::Mario(): GameObject(new MarioIdleState())
 {
+}
+
+void Mario::Update(float deltaTime)
+{
+	if (this->_velocity.x < 0)
+	{
+		this->_direction = DIRECTION::LEFT;
+	}
+	else if (this->_velocity.x > 0)
+	{
+		this->_direction = DIRECTION::RIGHT;
+	}
+	GameObject::Update(deltaTime);
 }
 
 void Mario::Render()
 {
 	VECTOR2D worldPosition = GetWorldPosition();
-	this->_state->GetAnimation()->Render(worldPosition.x, worldPosition.y, false);
+	bool isFlipped = this->_direction == DIRECTION::LEFT ? false : true;
+	this->_state->GetAnimation()->Render(worldPosition.x, worldPosition.y, isFlipped);
+}
+
+void Mario::OnKeyDown(int keyCode)
+{
+	switch (keyCode)
+	{
+	case DIK_UP:
+		DebugOut((wchar_t*)L"[INFO] Key input UP \n");
+		this->_state->Jump(-SPEED);
+		break;
+	case DIK_DOWN:
+		DebugOut((wchar_t*)L"[INFO] Key input DOWN \n");
+		this->_state->Sit();
+		break;
+	case DIK_LEFT:
+		DebugOut((wchar_t*)L"[INFO] Key input LEFT \n");
+		this->_state->Run(-SPEED);
+		break;
+	case DIK_RIGHT:
+		DebugOut((wchar_t*)L"[INFO] Key input RIGHT \n");
+		this->_state->Run(SPEED);
+		break;
+	default:
+		this->_state->Idle();
+		break;
+	}
+}
+
+void Mario::OnKeyUp(int keyCode)
+{
+	switch (keyCode)
+	{
+	case DIK_UP:
+	case DIK_DOWN:
+	case DIK_LEFT:
+	case DIK_RIGHT:
+		this->_state->Idle();
+		break;
+	default:
+		break;
+	}
 }
--- DirectXGame/Game/Mario/Mario.h
+++ DirectXGame/Game/Mario/Mario.h
@@ -1,11 +1,17 @@
 #pragma once
+#include <dinput.h>
 #include "../../Core/GameObject.h"
 
+#define SPEED 10
 class Mario: public GameObject
 {
 public:
 	Mario();
 
+	void Update(float deltaTime) override;
 	void Render() override;
+
+	void OnKeyDown(int keyCode) override;
+	void OnKeyUp(int keyCode) override;
 };
 
