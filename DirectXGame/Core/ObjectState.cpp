c08a683cfb1e99a2ed2a56df66ad2e3c904c0422
nguyentan2212 <nguyentan2212@gmail.com>
2022-10-26 21:51:18 +0700

    implement camera for bounding box

--- DirectXGame/Core/ObjectState.cpp
+++ DirectXGame/Core/ObjectState.cpp
@@ -1,5 +1,6 @@
 #include "ObjectState.h"
 #include "GameObject.h"
+#include "../Graphic/Graphic.h"
 
 ObjectState::~ObjectState()
 {
@@ -7,7 +8,9 @@ ObjectState::~ObjectState()
 
 Box ObjectState::GetBoundingBox()
 {
-	VECTOR2D result = this->_context->GetWorldPosition();
+	Graphic* graphic = Graphic::GetInstance();
+
+	VECTOR2D result = this->_context->GetWorldPosition() - graphic->cameraPosition;
 	result -= VECTOR2D(this->_width, this->_height) / 2;
 
 	return Box(result.x, result.y, this->_width, this->_height);
