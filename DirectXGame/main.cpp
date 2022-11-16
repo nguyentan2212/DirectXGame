cf2cfa7f9a7a943eef1a23d9e2c17b7f68b755e5
nguyentan2212 <nguyentan2212@gmail.com>
2022-11-16 14:59:54 +0700

    test world map scene

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,11 +1,13 @@
 #include <Windows.h>
 #include "Core/GameEngine.h"
 #include "Game/Scenes/CSceneOne.h"
+#include "Game/Scenes/CSceneWorldMap.h"
 
 #ifndef UNICODE
 #define UNICODE
 #endif
 
+#define SCENE_WORLD_MAP "Assets/Tilemaps/world-map.json"
 #define SCENE_ONE "Assets/Tilemaps/stage-1.json"
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
@@ -14,7 +16,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 	// Register the window class.
 	GameEngine* app = GameEngine::GetInstance();
 	app->Init(hInstance, nCmdShow);
-	app->AddScene(new CSceneOne(SCENE_ONE));
+	app->AddScene(new CSceneWorldMap(SCENE_WORLD_MAP));
 	app->Run();
 
 	return 0;
