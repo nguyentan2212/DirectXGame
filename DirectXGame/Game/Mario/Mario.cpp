ff861b3b196ea25ffe2fcb19a8c83abaed7e7037
nguyentan2212 <nguyentan2212@gmail.com>
2022-12-19 17:05:19 +0700

    add const for mario states

--- DirectXGame/Game/Mario/Mario.cpp
+++ DirectXGame/Game/Mario/Mario.cpp
@@ -1,17 +1,18 @@
 #include "Mario.h"
 #include "MarioIdleState.h"
 #include "MarioJumpState.h"
+#include "MarioFallState.h"
 #include "../../Physic/CollisionManager.h"
 
 Mario::Mario(): GameObject(new MarioIdleState())
 {
 	this->_showBoundingBox = true;
 	this->velocity = VECTOR2D(0.0f, -100.0f);
+	this->_name = "super mario";
 }
 
 void Mario::Update(float deltaTime)
 {
-	this->_isGrounded = false;
 	CollisionManager* collision = CollisionManager::GetInstance();
 	list<GameObject*> results = collision->RayCastWith(this, DIRECTION::DOWN, 10);
 	for (GameObject* obj : results)
@@ -19,14 +20,12 @@ void Mario::Update(float deltaTime)
 		if (obj->name == "ground" || obj->name == "panel" || obj->name == "pine")
 		{
 			this->_isGrounded = true;
-			DebugOut((wchar_t*)L"[INFO] Mario is GROUNDED \n");
 		}
 	}
-	if (this->_isGrounded == false)
+	if (this->_isGrounded == false && this->_state->name != "jump" && this->_state->name != "fall")
 	{
 		//not on ground
-		this->velocity = VECTOR2D(0.0f, -100.0f);
-		TransitionTo(new MarioJumpState());
+		TransitionTo(new MarioFallState());
 	}
 	if (this->_velocity.x < 0)
 	{
@@ -36,7 +35,14 @@ void Mario::Update(float deltaTime)
 	{
 		this->_direction = DIRECTION::RIGHT;
 	}
+	this->_state->Update(deltaTime);
 	GameObject::Update(deltaTime);
+	if (abs(this->_velocity.x) > MARIO_RUN_MAX_SPEED_X)
+	{
+		this->_velocity.x = this->_velocity.x > 0 ? MARIO_RUN_MAX_SPEED_X : -MARIO_RUN_MAX_SPEED_X;
+	}
+	this->_isGrounded = false;
+	//DebugOut((wchar_t*)L"[INFO] Mario vel.x = %f, vel.y = %f, acc.x = %f, acc.y = %f \n", _velocity.x, _velocity.y, _acceleration.x, _acceleration.y);
 }
 
 void Mario::Render()
@@ -58,12 +64,18 @@ void Mario::OnKeyUp(int keyCode)
 void Mario::OnCollision(CollisionEvent colEvent)
 {
 	this->_state->OnCollision(colEvent);
+	if (colEvent.collisionObj->name == "pine" && colEvent.direction != Direction::DOWN)
+	{
+		this->_position += this->_velocity * colEvent.entryTimePercent * colEvent.deltaTime / 1000;
+		this->_velocity = VECTOR2D(0, 0);
+		this->_acceleration = VECTOR2D(0.0f, 0.0f);
+	}
 	DebugOut((wchar_t*)L"[INFO] Collision entry time: %f, delta time: %f \n", colEvent.entryTimePercent, colEvent.deltaTime);
 }
 
 Animation* Mario::GetAnimation()
 {
-	string stateName = "super mario " + this->_state->name;
+	string stateName = this->_name + " " + this->_state->name;
 
 	AnimationService* animations = AnimationService::GetInstance();
 	return animations->GetAnimation(stateName);
--- DirectXGame/Game/Mario/Mario.h
+++ DirectXGame/Game/Mario/Mario.h
@@ -2,7 +2,13 @@
 #include <dinput.h>
 #include "../../Core/GameObject.h"
 
-constexpr float SPEED = 100;
+constexpr float MARIO_GRAVITY = 120;
+
+constexpr float MARIO_X_ACCE = 80;
+constexpr float MARIO_RUN_MAX_SPEED_X = 120;
+constexpr float MARIO_JUMP_SPEED_Y = 100;
+constexpr float MARIO_JUMP_DEFLECT_SPEED = 400;
+
 class Mario: public GameObject
 {
 public:
