9ddaf02f83dda5387466ffe4d209637ffd85fd41
nguyentan2212 <nguyentan2212@gmail.com>
2022-12-19 17:04:14 +0700

    fall state

---
+++ DirectXGame/Game/Mario/MarioFallState.cpp
@@ -0,0 +1,56 @@
+#include "MarioFallState.h"
+#include "MarioJumpState.h"
+#include "MarioIdleState.h"
+#include "MarioRunState.h"
+#include "../../Core/GameObject.h"
+#include "../../Core/KeyboardHandler.h"
+#include "Mario.h"
+
+MarioFallState::MarioFallState()
+{
+    this->_name = "fall";
+    DebugOut((wchar_t*)L"[INFO] Mario transition to Fall State \n");
+}
+
+void MarioFallState::OnTransition()
+{
+    if (this->_context->name == "small mario")
+    {
+        this->_width = 16.0f;
+        this->_height = 16.0f;
+    }
+    else
+    {
+        this->_width = 16.0f;
+        this->_height = 32.0f;
+    }
+    this->_context->velocity = VECTOR2D(this->_context->velocity.x, 0.0f);
+    this->_context->acceleration = VECTOR2D(this->_context->acceleration.x, - MARIO_GRAVITY);
+}
+
+void MarioFallState::Update(float deltaTime)
+{
+}
+
+void MarioFallState::OnCollision(CollisionEvent colEvent)
+{
+    string objName = colEvent.collisionObj->name;
+    if (colEvent.direction == DIRECTION::DOWN && objName == "pine" || objName == "ground")
+    {
+        KeyboardHandler* keyboard = KeyboardHandler::GetInstance();
+        if (keyboard->IsKeyDown(DIK_LEFT)) // prev was running left
+        {
+            this->_context->velocity = VECTOR2D(-abs(this->_context->velocity.x), 0.0f);
+            this->_context->TransitionTo(new MarioRunState(-1));
+        }
+        else if(keyboard->IsKeyDown(DIK_RIGHT)) // prev was running left
+        {
+            this->_context->velocity = VECTOR2D(abs(this->_context->velocity.x), 0.0f);
+            this->_context->TransitionTo(new MarioRunState(-1));
+        }
+        else
+        {
+            this->_context->TransitionTo(new MarioIdleState());
+        }
+    }
+}
---
+++ DirectXGame/Game/Mario/MarioFallState.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "../../Core/ObjectState.h"
+
+class MarioFallState: public ObjectState
+{
+public:
+	MarioFallState();
+	void OnTransition() override;
+	void Update(float deltaTime) override;
+	void OnCollision(CollisionEvent colEvent) override;
+};
+
