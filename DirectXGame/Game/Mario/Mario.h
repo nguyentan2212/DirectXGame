7fd29a7ad61fd41cf8edf998d24d1671199bd7c8
nguyentan2212 <nguyentan2212@gmail.com>
2022-12-20 15:14:26 +0700

    update jump speed

--- DirectXGame/Game/Mario/Mario.h
+++ DirectXGame/Game/Mario/Mario.h
@@ -6,7 +6,7 @@ constexpr float MARIO_GRAVITY = 120;
 
 constexpr float MARIO_X_ACCE = 80;
 constexpr float MARIO_RUN_MAX_SPEED_X = 120;
-constexpr float MARIO_JUMP_SPEED_Y = 100;
+constexpr float MARIO_JUMP_SPEED_Y = 120;
 constexpr float MARIO_JUMP_DEFLECT_SPEED = 400;
 
 class Mario: public GameObject
