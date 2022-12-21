bee369f02965b524188bda0af674f753825933fb
nguyentan2212 <nguyentan2212@gmail.com>
2022-12-21 15:45:39 +0700

    remove redundant class

--- DirectXGame/Core/EmptyObjectState.cpp
+++
@@ -1,7 +0,0 @@
-#include "EmptyObjectState.h"
-
-EmptyObjectState::EmptyObjectState(float width, float height)
-{
-	this->_width = width;
-	this->_height = height;
-}
--- DirectXGame/Core/EmptyObjectState.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ObjectState.h"
-
-class EmptyObjectState: public ObjectState
-{
-public:
-	EmptyObjectState(float width, float height);
-};
-
