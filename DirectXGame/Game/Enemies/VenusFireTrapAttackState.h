61b951a48a36c44ff38f544d3b1df273802a30bd
nguyentan2212 <nguyentan2212@gmail.com>
2023-01-05 15:24:53 +0700

    update distance detect mario and fire ball speed

--- DirectXGame/Game/Enemies/VenusFireTrapAttackState.h
+++ DirectXGame/Game/Enemies/VenusFireTrapAttackState.h
@@ -2,9 +2,9 @@
 #include "../../Core/ObjectState.h"
 
 constexpr float FIRE_TRAP_Y_SPEED = 30;
-constexpr float FIRE_TRAP_X_DISTANCE_DETECT = 100;
+constexpr float FIRE_TRAP_X_DISTANCE_DETECT = 80;
 constexpr float FIRE_TRAP_ATTACK_COOLDOWN = 2000;
-constexpr float FIRE_BALL_SPEED = 70;
+constexpr float FIRE_BALL_SPEED = 50;
 constexpr float FIRE_BALL_MAX_DISTANCE = 600;
 
 class VenusFireTrapAttackState: public ObjectState
